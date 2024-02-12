f2ffd421db839c1898a0fbd520dd91c73458aefe
Imanol <20230065@ricaldone.edu.sv>
2024-02-12 11:13:02 -0600

    Agregué una alerta

---
+++ script.js
@@ -0,0 +1,9 @@
+import Swal from 'sweetalert2'
+
+Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Your work has been saved",
+    showConfirmButton: false,
+    timer: 2500
+  })
