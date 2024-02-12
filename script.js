ae82352785b6896352839d90355390c66b7c9d74
Imanol <20230065@ricaldone.edu.sv>
2024-02-12 11:23:07 -0600

    Agregué una pregunta

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import Swal from 'sweetalert2'
-
+//Alert
 Swal.fire({
     position: "center",
     icon: "success",
@@ -7,3 +7,41 @@ Swal.fire({
     showConfirmButton: false,
     timer: 2500
   })
+
+  //Prompt
+  Swal.fire({
+    title: "Ingresa tu nombre:",
+    input: "text",
+    inputAttributes: {
+      autocapitalize: "Cancelar"
+    },
+    showCancelButton: true,
+    confirmButtonText: "Aceptar",
+    showLoaderOnConfirm: true,
+    preConfirm: async (login) => {
+      try {
+        const githubUrl = `
+          https://api.github.com/users/${login}
+        `;
+        const response = await fetch(githubUrl);
+        if (!response.ok) {
+          return Swal.showValidationMessage(`
+            ${JSON.stringify(await response.json())}
+          `);
+        }
+        return response.json();
+      } catch (error) {
+        Swal.showValidationMessage(`
+          Request failed: ${error}
+        `);
+      }
+    },
+    allowOutsideClick: () => !Swal.isLoading()
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: `${result.value.login} este es tu avatar según tu nombre :D`,
+        imageUrl: result.value.avatar_url
+      });
+    }
+  });
